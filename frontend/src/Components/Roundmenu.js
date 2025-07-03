aac066a642e5ef79fd23f738857e393bbacdf1ff
unknown <nestorortiz017@gmail.com>
2025-07-03 17:51:12 -0500

    react front removed

--- frontend/src/Components/Roundmenu.js
+++
@@ -1,9 +0,0 @@
-import './Roundmenu.css'
-
-export default function Roundmenu(props){
-    return (
-    <div ref = {props.ref} style = {props.style} className= {`menubg ${props.className}`}>
-        {props.children}
-    </div>
-    );
-}
